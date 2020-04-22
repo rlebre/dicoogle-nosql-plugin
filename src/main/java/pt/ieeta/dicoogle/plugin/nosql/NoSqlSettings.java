108be8470a9174563551b27b829674f6f0d4544e
Rui Lebre <ruilebre@ua.pt>
2020-04-22 13:02:54 +0100

    Created default settings class

---
+++ src/main/java/pt/ieeta/dicoogle/plugin/nosql/NoSqlSettings.java
@@ -0,0 +1,48 @@
+package pt.ieeta.dicoogle.plugin.nosql;
+
+public class NoSqlSettings {
+    private static NoSqlSettings instance = null;
+    private String host = "localhost";
+    private int port = 27017;
+    private String dbName = "DicoogleDatabase";
+    private String collectionName = "DicoogleObjs";
+
+    public static NoSqlSettings getInstance() {
+        if (instance == null)
+            instance = new NoSqlSettings();
+        return instance;
+
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+}
